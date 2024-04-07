ef858ccd1db83bf138601ca755ace4a83c876f16
채종민 <54351019+okck123@users.noreply.github.com>
2024-04-07 18:42:02 +0900

    Update 조건에 맞게 수열 변환하기 2.js
    설명추가

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,6 +1,8 @@
 // 1. 현재 arr을 저장할 arr이 필요함
 // 2. 현재 arr과 prev arr이 같아지는 시점의 count값을 반환하자
 // 3. 전체 배열의 값들이 다 변해야 하니 map 사용
+// 4. arr(x) = arr(x+1)이 항상 존재하므로 1번이상은 작업을 반복한다.
+// 5. 전과 현재를 비교하기 때문에 count의 초기값은 -1로 지정하고 실행.
 function solution(arr) {
     var answer = -1;
     let prevarr = [];
