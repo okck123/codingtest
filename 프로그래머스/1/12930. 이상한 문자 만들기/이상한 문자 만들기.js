a71984a6640f62323a86f305b21119fe41d90eb4
채종민 <54351019+okck123@users.noreply.github.com>
2024-04-09 17:37:57 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -2,17 +2,32 @@
 //index 홀수 => 소문자
 //공백을 기준으로 index는 초기화됨
 function solution(s) {
-    var answer = '';
     let index = 0;
+    var answer = s.split('').map(e=>{
+            if(e === ' '){
+                index = 0;
+            }else{
+                if(index%2===0){
+                    e = e.toUpperCase();
+                    index++;
+                }else{
+                    e = e.toLowerCase();
+                    index++;
+                    
+                }
+            }
+            return e;
+        }).join('');
     
-    s.split('').forEach(e=>{
-        if(e === ' '){
-            answer += e;
-            index = 0;
-        }else{
-            index%2===0 ? answer += e.toUpperCase() : answer += e.toLowerCase();
-            index++;
-        }
-    })
+    
+    // s.split('').forEach(e=>{
+    //     if(e === ' '){
+    //         answer += e;
+    //         index = 0;
+    //     }else{
+    //         index%2===0 ? answer += e.toUpperCase() : answer += e.toLowerCase();
+    //         index++;
+    //     }
+    // })
     return answer;
 }
