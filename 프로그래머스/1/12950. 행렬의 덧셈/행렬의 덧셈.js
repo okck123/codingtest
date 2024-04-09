1fca2b6727bdc53f2cbaefc388cf785827c69f42
채종민 <54351019+okck123@users.noreply.github.com>
2024-04-09 15:39:44 +0900

    [level 1] Title: 행렬의 덧셈, Time: 13.42 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,10 @@
 function solution(arr1, arr2) {
-    var answer = [];
+    var answer = [[]];
     for(let i = 0 ; i<arr1.length;i++){
-        const arr = [];
+        answer[i] = [];
         for(let j = 0; j<arr1[i].length;j++){
-            arr.push(arr1[i][j]+arr2[i][j])
+            answer[i].push(arr1[i][j]+arr2[i][j])
         }
-        answer.push(arr);
         // console.log([arr1[i][0]+arr2[i][0],arr1[i][1]+arr2[i][1]]);
     }
     // console.log(arr1.length,arr1[0].length)
