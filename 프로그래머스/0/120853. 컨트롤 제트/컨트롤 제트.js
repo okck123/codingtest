8881ae2c970424816d0266937523e2a053a0e513
채종민 <54351019+okck123@users.noreply.github.com>
2024-04-09 14:08:07 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -3,7 +3,7 @@ function solution(s) {
     var answer = 0;
     let arr = [];
     s.split(' ').forEach(e=>{
-        e === 'Z' ? arr.pop() : arr.push(e);
+        e === 'Z' ? arr.pop() : arr.push(+e);
     })
-    return arr.reduce((acc,cur)=> acc + +cur,0);
+    return arr.reduce((acc,cur)=> acc+cur,0);
 }
