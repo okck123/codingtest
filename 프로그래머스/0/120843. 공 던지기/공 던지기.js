f8f3e1816a54d30d98b423cafe728d30e3b0b084
채종민 <54351019+okck123@users.noreply.github.com>
2024-04-09 14:33:47 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,7 +1,7 @@
 function solution(numbers, k) {
-    var answer = [...numbers];
-    while(answer.length<2*k+1){
-        answer = [...answer,...numbers];
-    }
-    return answer[2*(k-1)];
+    // var answer = [...numbers];
+    // while(answer.length<2*k+1){
+    //     answer = [...answer,...numbers];
+    // }
+    return numbers[2*(k-1)%numbers.length];
 }
